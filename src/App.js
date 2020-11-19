6b93d8bb638f974cc16a85f988607ee03229af7f
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2020-11-19 19:24:54 +0100

    🌐

--- src/App.js
+++ src/App.js
@@ -149,6 +149,7 @@ export default function App() {
             />
             <div className="sidemenu">
               <SetControlColormap
+                className="colormap"
                 colormaps="all"
                 colormap={colormap}
                 onSetChange={(ev) => setColormap(ev.detail)}

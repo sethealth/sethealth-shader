dbd1674cc9ee62cfe4948f56da00096d785d5767
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2020-11-19 23:58:33 +0100

    🍿

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ export default function App() {
             </select>
           )}
           <nav className="top-menu">
-            <a class="link" href="https://docs.set.health">
+            <a class="link" href="https://docs.set.health/docs/guides/custom-shaders">
               <SetIcon name="document"></SetIcon>
               Docs
             </a>
--- src/code.js
+++ src/code.js
@@ -31,38 +31,6 @@ void main() {
     maxDensity /= highCut-lowCut;
     gl_FragColor = readColormap(maxDensity);
 }
-
-/**
-    struct Ray {
-        vec3 start;     // start point of the ray caster
-        vec3 end;       // end point of the ray caster
-        vec3 delta;     // delta vector of each iteration
-        vec3 direction; // normalized direction of the eye ray
-        int steps;      // number of steps of our ray caster
-        bool outside;   // "true" when the ray is outside the volume
-    };
-
-    Ray computeRay()
-    Creates a Ray struct, containing all the ray properties required to perform a ray casting.
-
-    float readVolume(vec3 cursor)
-    Returns the normalized pixel value (density) of our volume at the specified 3D point.
-
-    vec3 readNormal(vec3 cursor)
-    Returns the normalized normal vector at the specified 3D point.
-    Normals are computed as the gradient of the volume
-
-    vec4 readColormap(float density)
-    Resolves the color vector for the specified density value in thee [0, 1].
-
-    vec4 readColor(vec3 cursor)
-    Resolves the color vector based on the density at the specified 3D point.
-
-    float depthAt(vec3 cursor)
-    Returns the non-linear depth at the specified 3D point.
-    This value can be passed directly to gl_FragDepth.
-*/
-
 `;
 
 
